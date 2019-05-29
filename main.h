2b8c7cce025e821e2af3f9eebf9399cf6bab218c
RokasT <rokas.tamulevicius28@gmail.com>
2019-05-29 09:06:48 +0300

    update

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef VUOBJPROG5_MAIN_H
+#define VUOBJPROG5_MAIN_H
+
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+#define data "tekstas.txt"
+
+#endif //VUOBJPROG5_MAIN_H
